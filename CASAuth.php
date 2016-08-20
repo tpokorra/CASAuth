ec766caef56a735fe2f8bf2be29d1836125576b4
Jeffrey Gill <jpg18@case.edu>
2016-08-19 21:07:21 -0400

    Prevent phpCAS from creating a new, conflicting session
    Elimiates the issue described in commit b0277f6c for MW 1.27, in which
    an "Invalid session ID" exception is thrown when attempting to log in.
    The phpCAS client constructor calls session_start(), which will
    overwrite any existing session data if not protected. This was only
    sometimes an issue since existing session data is not always present,
    depending on modifications made to the installation (e.g., loading
    certain extensions).
    
    This change was tested on MW 1.23 and appears to be backwards
    compatible.

--- CASAuth.php
+++ CASAuth.php
@@ -275,6 +275,9 @@ function casSetup() {
         global $CASAuth;
         global $casIsSetUp;
 
+        // Make the session persistent so that phpCAS doesn't change the session id
+        wfSetupSession();
+
         require_once($CASAuth["phpCAS"]."/CAS.php");
         phpCAS::client($CASAuth["Version"], $CASAuth["Server"], $CASAuth["Port"], $CASAuth["Url"], false);
         phpCAS::setSingleSignoutCallback('casSingleSignOut');
